e0c8840b14e4f3842cc7f988a7bb2fa59ac2317f
Jory Roberts <robertsjory13@gmail.com>
2023-09-09 23:05:59 -0500

    completes lab

--- server/models.py
+++ server/models.py
@@ -1,23 +1,38 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import validates
+
 db = SQLAlchemy()
 
+
 class Author(db.Model):
-    __tablename__ = 'authors'
-    # Add validations and constraints 
+    __tablename__ = "authors"
+    # Add validations and constraints
 
     id = db.Column(db.Integer, primary_key=True)
-    name= db.Column(db.String, unique=True, nullable=False)
+    name = db.Column(db.String, unique=True, nullable=False)
     phone_number = db.Column(db.String)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
+    @validates("name")
+    def validate_name(self, key, value):
+        if value == "":
+            raise ValueError("Name cannot be blank")
+        return value
+
+    @validates("phone_number")
+    def validate_phone_number(self, key, value):
+        if len(value) != 10:
+            raise ValueError("Phone number must be 10 digits")
+        return value
+
     def __repr__(self):
-        return f'Author(id={self.id}, name={self.name})'
+        return f"Author(id={self.id}, name={self.name})"
+
 
 class Post(db.Model):
-    __tablename__ = 'posts'
-    # Add validations and constraints 
+    __tablename__ = "posts"
+    # Add validations and constraints
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
@@ -27,6 +42,29 @@ class Post(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
+    @validates("content")
+    def validate_content(self, key, value):
+        if len(value) < 250:
+            raise ValueError("Content must be 250 characters")
+        return value
+
+    @validates("summary")
+    def validate_summary(self, key, value):
+        if len(value) >= 250:
+            raise ValueError("Summary cannot be more than 250 characters")
+        return value
+
+    @validates("category")
+    def validate_category(self, key, value):
+        if value not in ["Fiction", "Non-Fiction"]:
+            raise ValueError("Category is not in either Fiction or Non-Fiction")
+        return value
+
+    @validates("title")
+    def validates_title(self, key, value):
+        if value not in ["Won't Believe", "Secret", "Top", "Guess"]:
+            raise ValueError("Title is incorrect")
+        return value
 
     def __repr__(self):
-        return f'Post(id={self.id}, title={self.title} content={self.content}, summary={self.summary})'
+        return f"Post(id={self.id}, title={self.title} content={self.content}, summary={self.summary})"
